ba00658aa3a323c0152b3b392ecec49b92138243
Aruna Benjamin <arunabenji29@gmail.com>
2019-10-04 11:46:41 -0600

    completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,8 +12,53 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    answer = []
+    retrieve = ht.storage
+    for weight in weights:
+        hash_table_insert(ht,weight,weight)
 
-    return None
+    if length > 1:
+
+
+        for i in range(len(retrieve)):
+            first_el = None
+            second_el = None
+            first_i = None
+            second_i = None
+            retrieve_first = None
+            retrieve_second = None
+
+            if retrieve[i] is not None:
+                first_el = retrieve[i].key
+                second_el = limit - first_el
+                retrieve_first = hash_table_retrieve(ht,first_el)
+                retrieve_second = hash_table_retrieve(ht,second_el)
+
+            if retrieve_first is not None and retrieve_second is not None and retrieve_first + retrieve_second == limit :
+                
+                if retrieve_first == retrieve_second :
+                    result = [i for i, e in enumerate(weights) if e == retrieve_first]
+                    if result[0] >= result[1]:
+                        answer.append(result[0])
+                        answer.append(result[1])
+                    else:
+                        answer.append(result[1])
+                        answer.append(result[0])
+                    return answer
+
+                first_i = weights.index(retrieve_first)
+                second_i = weights.index(retrieve_second)
+
+                if first_i >= second_i:
+                    answer.append(first_i)
+                    answer.append(second_i)
+                else:                    
+                    answer.append(second_i)
+                    answer.append(first_i)
+                return answer
+            
+    else:
+        return None
 
 
 def print_answer(answer):
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -19,5 +19,18 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    for i in tickets:
+        # print(f'source: {i.source}')
+        # print(f'destination: {i.destination}')
+        hash_table_insert(hashtable,i.source,i.destination)
+
+    count=0
+    prev= "NONE"
+    while count<length:
+        
+        route[count]= hash_table_retrieve(hashtable,prev)
+        prev = route[count]
+        count += 1
+        
 
     return route
